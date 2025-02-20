aae166d78190931a27a4afd6a02e330e9207979a
Anthony Liao <anliao@umich.edu>
2025-02-20 11:22:01 -0500

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyCzY2xWB-R3hG92gxdGOazkVv5RMD2VVso",
+    authDomain: "friendlychat-9bfaf.firebaseapp.com",
+    projectId: "friendlychat-9bfaf",
+    storageBucket: "friendlychat-9bfaf.firebasestorage.app",
+    messagingSenderId: "974238913409",
+    appId: "1:974238913409:web:74050be0aaf833c1288e80"
   },
 };
